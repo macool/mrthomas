d67dc28b6d55e0a476a2cc62028f9fc088d6b88f
Mario Andrés Correa A <a@macool.me>
2021-02-06 23:37:06 -0500

    bootstrapping

--- lib/mrthomas_web/controllers/page_controller.ex
+++ lib/mrthomas_web/controllers/page_controller.ex
@@ -2,6 +2,12 @@ defmodule MrthomasWeb.PageController do
   use MrthomasWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    {:ok, body} = File.read("pairs.txt")
+    pairs = :binary.split(body, "\n", [:global, :trim])
+    render(conn, "index.html", pairs: pairs)
+  end
+
+  def show(conn, %{ "id" => id }) do
+    render(conn, "show.html", pair: id)
   end
 end
